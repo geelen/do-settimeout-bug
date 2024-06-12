c8ecdeb20264701c84bcfa8c477d57b46d12cbc2
Glen Maddern <glen@cloudflare.com>
2024-06-12 14:29:42 +1000

    Looks like I can't reproduce the bug!

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { DurableObject } from "cloudflare:workers";
+import { DurableObject } from 'cloudflare:workers'
 
 /**
  * Welcome to Cloudflare Workers! This is your first Durable Objects application.
@@ -13,49 +13,67 @@ import { DurableObject } from "cloudflare:workers";
  * Learn more at https://developers.cloudflare.com/durable-objects
  */
 
-
 /**
  * Associate bindings declared in wrangler.toml with the TypeScript type system
  */
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	MY_DURABLE_OBJECT: DurableObjectNamespace<MyDurableObject>;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	MY_DURABLE_OBJECT: DurableObjectNamespace<MyDurableObject>
 }
 
 /** A Durable Object's behavior is defined in an exported Javascript class */
-export class MyDurableObject extends DurableObject {
-	/**
-	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
-	 * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
-	 *
-	 * @param ctx - The interface for interacting with Durable Object state
-	 * @param env - The interface to reference bindings declared in wrangler.toml
-	 */
-	constructor(ctx: DurableObjectState, env: Env) {
-		super(ctx, env);
+export class MyDurableObject extends DurableObject<Env> {
+	object_instantiation_time = performance.now()
+	private human_ids: Record<string, string> = {}
+	private name?: string
+
+	async sayHello(name: string, who: string): Promise<string> {
+		this.name = name
+
+		this.log(`Hi from now.`)
+		for (let i = 1; i <= 200; i++) {
+			setTimeout(() => {
+				this.log(`Hi from ${i * 5} seconds ago.`)
+			}, i * 5_000)
+		}
+		return `Hello, ${who}!`
 	}
 
-	/**
-	 * The Durable Object exposes an RPC method sayHello which will be invoked when when a Durable
-	 *  Object instance receives a request from a Worker via the same method invocation on the stub
-	 *
-	 * @param name - The name provided to a Durable Object instance from a Worker
-	 * @returns The greeting to be sent back to the Worker
-	 */
-	async sayHello(name: string): Promise<string> {
-		return `Hello, ${name}!`;
+	// Get a short (8 char) hash of an id so we can track if there's more than one object with the same ID
+	private async getHumanInstanceId(input: string) {
+		return (this.human_ids[input] ??= btoa(
+			Array.from(
+				new Uint32Array(
+					await crypto.subtle.digest(
+						'SHA-256',
+						new TextEncoder().encode(input),
+					),
+				),
+			).join(''),
+		).slice(-10, -2))
+	}
+
+	private async log(str: string) {
+		const raw_object_id = this.ctx.id.toString()
+		const object_id = await this.getHumanInstanceId(raw_object_id)
+		const raw_instance_id = this.object_instantiation_time.toString()
+		const instance_id = await this.getHumanInstanceId(raw_instance_id)
+		const message = `[${this.name} • ${object_id} • ${instance_id}] ${str}`
+		console.log(message)
+		await fetch(`https://events.baselime.io/v1/logs`, {
+			method: 'POST',
+			headers: {
+				'x-api-key': 'e673cb41a6638eada520defb90b236b9aa257d53',
+				'content-type': 'application/json',
+				'x-service': 'do-settimeout-bug',
+			},
+			body: JSON.stringify([
+				{
+					message,
+					namespace: this.name,
+					data: { object_id, raw_object_id, instance_id, raw_instance_id },
+				},
+			]),
+		})
 	}
 }
 
@@ -68,19 +86,24 @@ export default {
 	 * @param ctx - The execution context of the Worker
 	 * @returns The response to be sent back to the client
 	 */
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext,
+	): Promise<Response> {
 		// We will create a `DurableObjectId` using the pathname from the Worker request
 		// This id refers to a unique instance of our 'MyDurableObject' class above
-		let id: DurableObjectId = env.MY_DURABLE_OBJECT.idFromName(new URL(request.url).pathname);
+		let name = new URL(request.url).pathname
+		let id: DurableObjectId = env.MY_DURABLE_OBJECT.idFromName(name)
 
 		// This stub creates a communication channel with the Durable Object instance
 		// The Durable Object constructor will be invoked upon the first call for a given id
-		let stub = env.MY_DURABLE_OBJECT.get(id);
+		let stub = env.MY_DURABLE_OBJECT.get(id)
 
 		// We call the `sayHello()` RPC method on the stub to invoke the method on the remote
 		// Durable Object instance
-		let greeting = await stub.sayHello("world");
+		let greeting = await stub.sayHello(name, 'world')
 
-		return new Response(greeting);
+		return new Response(greeting)
 	},
-};
+}
